d6c3cc7a49fde41f893702085163cdaf4570d361
chromodyne <dsinitiere@outlook.com>
2022-07-31 19:44:48 -0700

    Setup essential middleware functions for express. Setup handlebars for later use.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# tech_blog
+# tech_blog #
+
+**Note:** This repository contains the source code for my full-stack Tech Blog website created for Challenge 14 of GA Tech's 2022 Coding Bootcamp.
+
+## Description ##
+
+## Walkthrough Video ##
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
-//const routes = require("./controllers");
+const routes = require("./controllers");
+const path = require("path");
 
 const sequelize = require("./config/connection.js");
 
@@ -10,6 +11,15 @@ const PORT = process.env.PORT || 3001;
 
 //Setup handlebars. No helpers currently.
 const hbs = exphbs.create({});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => { console.log(`App listening on: localhost:${PORT}`) })
