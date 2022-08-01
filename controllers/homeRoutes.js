10bff8256d5bbaa6af27fa5d23603e5b25b5881f
chromodyne <dsinitiere@outlook.com>
2022-07-31 20:43:56 -0700

    Added login page basic forms and added nav bar linking.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -14,4 +14,17 @@ router.get("/", async (req, res) => {
 
 });
 
+router.get("/login", async (req, res) => {
+
+    try {
+
+        res.render("login");
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+
+
+});
+
 module.exports = router;
